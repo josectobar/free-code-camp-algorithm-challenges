3231b5d30cac13b74f45627520527a15727ac2fc
Jose Tobar <josectobar@gmail.com>
2019-01-18 13:10:05 -0700

    Added new algorithm challenges.

--- JSlessons.js
+++ JSlessons.js
@@ -341,3 +341,308 @@ because it contains the name and its value, that was
 passed on as the second argument.
 
 */
+
+function whatIsInAName(collection, source) {
+  // What's in a name?
+  var arr = [];
+  // Only change code below this line
+  let keys = Object.keys(source);
+  arr = collection.filter(function(obj) {
+    let colKey = Object.keys(obj)
+    for (let i = 0; i < keys.length; i++)  {
+      if (colKey.indexOf(keys[i]) === -1) {
+        return false
+      }
+      if (obj[colKey[colKey.indexOf(keys[i])]] !== source[keys[keys.indexOf(keys[i])]]) {
+        return false
+      }
+    }
+    return obj
+  })
+  console.log(arr)
+  // Only change code above this line
+  return arr;
+}
+
+whatIsInAName([{"a": 1, "b": 2, "c": 3}], {"a": 1, "b": 9999, "c": 3});
+
+//-------------------------------------------------
+
+
+/*
+
+Spinal Tap Case
+Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+
+*/
+
+function spinalCase(str) {
+  let arr = [];
+  arr = str.replace(/([a-z])([A-Z])/g, '$1 $2') === str ? str.replace(/_/g, " ").split(" ") : str.replace(/([a-z])([A-Z])/g, '$1 $2').split(" ")
+  return arr.join("-").toLowerCase();
+}
+spinalCase("ThisIsSpinalTap");
+
+//-------------------------------------------------
+
+/*
+
+Pig Latin
+Translate the provided string to pig latin.
+
+Pig Latin takes the first consonant (or consonant cluster) of an English word, moves it to the end of the word and suffixes an "ay".
+
+If a word begins with a vowel you just add "way" to the end.
+
+Input strings are guaranteed to be English words in all lowercase.
+
+
+*/
+
+function translatePigLatin(str) {
+  let vowelRegex = /[aeiou]/i; 
+  if (str.search(vowelRegex) === -1) {
+    return str.concat("ay")
+  }
+  return str.search(vowelRegex) === 0 ? str.concat("way") : str.slice(str.search(vowelRegex)).concat(str.slice(0, str.search(vowelRegex)) + "ay")
+}
+
+//-------------------------------------------------
+
+/*
+
+Search and Replace
+Perform a search and replace on the sentence using the arguments provided and return the new sentence.
+
+First argument is the sentence to perform the search and replace on.
+
+Second argument is the word that you will be replacing (before).
+
+Third argument is what you will be replacing the second argument with (after).
+
+Note
+Preserve the case of the first character in the original word when you are replacing it. 
+For example if you mean to replace the word "Book" with the word "dog", it should be replaced as "Dog"
+
+*/
+
+function myReplace(str, before, after) {
+  let regex = new RegExp(before, "i")
+  let firstChar = str.charAt(str.indexOf(str.match(regex)))
+  if (firstChar === firstChar.toUpperCase()) {
+    after = after.charAt(0).toUpperCase().concat(after.slice(1))
+  }
+  return str.replace(regex, after)
+}
+
+myReplace("A quick brown fox Jumped over the lazy dog", "jumped", "leaped");
+
+//-------------------------------------------------
+
+/*
+
+DNA Pairing
+The DNA strand is missing the pairing element. Take each character, get its pair, and return the results as a 2d array.
+
+Base pairs are a pair of AT and CG. Match the missing element to the provided character.
+
+Return the provided character as the first element in each array.
+
+For example, for the input GCG, return [["G", "C"], ["C","G"],["G", "C"]]
+
+The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array.
+
+
+*/
+
+function pairElement(str) {
+  let pair = [...str];
+  console.log(pair)
+  const convert = {
+      G: ["G", "C"],
+      C: ["C", "G"],
+      T: ["T", "A"],
+      A: ["A", "T"]
+  };
+    return pair.map( (elem) => convert[elem]);
+}
+
+pairElement("GCG");
+
+//-------------------------------------------------
+
+/*
+
+Missing letters
+Find the missing letter in the passed letter range and return it.
+
+If all letters are present in the range, return undefined.
+
+*/
+
+function fearNotLetter(str) {
+  let arr = []
+  for (let i = 0 ; i < str.length; i++ ) {
+      arr.push(str.charCodeAt(i));
+  }
+ return arr.reduce((x,y) =>  {
+      if (x - y === - 2) {
+          return String.fromCharCode(x + 1);
+      } 
+      return y;
+      })
+}
+
+//-------------------------------------------------
+
+/*
+
+ Sorted Union
+Write a function that takes two or more arrays and returns a new array 
+of unique values in the order of the original provided arrays.
+
+In other words, all values present from all arrays should be included in 
+their original order, but with no duplicates in the final array.
+
+The unique numbers should be sorted by their original order, but the final 
+array should not be sorted in numerical order.
+
+*/
+
+function uniteUnique() {
+  let newArr =[];
+  for (let i = 0; i < arguments.length; i++) {
+      newArr.push(...arguments[i])
+      console.log(arguments[i])
+  }
+  let unique = [...new Set(newArr)]
+  return unique
+}
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+
+// ---------------------
+
+/*
+
+Convert HTML Entities
+Convert the characters &, <, >, " (double quote), and ' (apostrophe), 
+in a string to their corresponding HTML entities.
+ 
+*/
+
+function convertHTML(str) {
+  // &colon;&rpar;
+  let regex = /<|>|"|'|&/g;
+  let refObj = {
+      "<": "&lt;",
+      ">": "&gt;",
+      '"': "&quot;",
+      "'": "&apos;",
+      "&": "&amp;"
+  }
+  return regex[Symbol.replace](str, (val) => {
+      return refObj[val]
+  })
+}
+
+convertHTML("Dolce & Gabbana");
+
+//-------------------------------------------------
+
+/*
+
+Sum All Odd Fibonacci Numbers
+
+Given a positive integer num, return the sum of all odd Fibonacci numbers 
+that are less than or equal to num.
+
+The first two numbers in the Fibonacci sequence are 1 and 1. Every additional 
+number in the sequence is the sum of the two previous numbers. The first six 
+numbers of the Fibonacci sequence are 1, 1, 2, 3, 5 and 8.
+
+For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less 
+than or equal to 10 are 1, 1, 3, and 5.
+
+
+*/
+
+function sumFibs(num) {
+  let oddSum = 1;
+  let fibCount = [1];
+  for (let i = 1; i <= num; i += fibCount.shift()) {
+      console.log(i)
+      if (i % 2 !== 0) {
+          console.log(i)
+          oddSum += i;
+      }
+      fibCount.push(i)
+  }
+  return oddSum;
+}
+
+sumFibs(75024);
+
+//-------------------------------------------------
+
+/*
+
+Drop it
+Given the array arr, iterate through and remove each element 
+starting from the first element (the 0 index) until the function 
+func returns true when the iterated element is passed through it.
+
+Then return the rest of the array once the condition is satisfied, 
+otherwise, arr should be returned as an empty array.
+
+*/
+
+function dropElements(arr, func) {
+  let newArr = arr;
+  let val;
+  // perform while loop as long as function is false and array length is not zero.
+  while (!func(val) && newArr.length != 0) {
+    // When function if false, remove first element of the array
+          if (!func(newArr[0])) {
+              newArr.shift()
+          }
+    // when function is true, set val to first element of array to end while loop.
+          if (func(newArr[0])) {
+              console.log(newArr[0])
+              val = newArr[0]
+          }
+  }
+  return newArr;
+}
+
+dropElements([1, 2, 3, 9, 2], function(n) {return n > 2;});
+
+//-------------------------------------------------
+
+/*
+
+Steamroller
+Flatten a nested array. You must account for varying levels of nesting.
+
+*/
+
+function steamrollArray(arr) {
+  let flatArray =[];
+  for (let i = 0; i < arr.length; i++) {
+
+      if ( !Array.isArray(arr[i]) ) {
+
+          flatArray.push(arr[i]);
+
+      } if ( Array.isArray( arr[i] ) ) {
+
+          let nestedArr = steamrollArray(arr[i]);
+          for (let i= 0; i < nestedArr.length; i++) {
+                  flatArray.push(nestedArr[i]);
+          }
+      }
+  }
+  return flatArray
+  
+}
+
+steamrollArray([5, [[4, 5, 6]], 5]);
